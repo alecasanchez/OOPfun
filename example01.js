596ee80d6391f223e0b7f2898a53d6e03baa3910
alecasanchez <alecsanchez162@gmail.com>
2023-03-26 15:28:01 -0500

    working

---
+++ example01.js
@@ -0,0 +1,11 @@
+// object literal
+const circle = {
+  radius: 1,
+  location: {
+    x: 1,
+    y: 1,
+  },//commas are extremely important!
+  draw: function() {
+    console.log("draw");
+  }
+};
